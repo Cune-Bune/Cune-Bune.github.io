550f4e430ed46be0d7b8637ef05ae9f5b6ae940c
rosvany02 <rosvany.rodri@gmail.com>
2019-10-15 11:11:49 -0400

    Temporary Game Outline Added

---
+++ Temporary Game 10.15/Assets/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 2.0f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rb.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) * speed);
+    }
+
+[RequireComponent(typeof(Rigidbody))]
+public class Jump : MonoBehaviour
+{
+    public float jumpSpeed = 5f;
+    public bool isGrounded;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ("Ground") && isGrounded == false)
+        {
+            isGrounded = true;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKey("q") || Input.GetMouseButton(0) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, 2, 0) * jumpSpeed, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+}
+    public class DestroyByContact : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Boundary")
+            {
+                return;
+            }
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
