2bc8df47fb0f547934cc58170f1fab969295fc70
Cune-Bune <Cune-Bune@users.noreply.github.com>
2019-10-23 14:20:29 -0400

    Re-added Jump

---
+++ Temporary Game 10.15/Assets/Jump.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Jump : MonoBehaviour
+{
+    public bool isGrounded;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ("Ground") && isGrounded == false)
+        {
+            isGrounded = true;
+        }
+        if (col.gameObject.tag == ("Jumpable") && isGrounded == false)
+        {
+            isGrounded = true;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+}
