1f0c7ee6184843a6c3c2f24c6fb511edcdc6c819
Cune-Bune <Cune-Bune@users.noreply.github.com>
2019-10-30 15:59:33 -0400

    Added LAZER

---
+++ Temporary Game 10.15/Assets/Laser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {  if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit = new RaycastHit();
+            Ray laser = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(laser, out hit, 10000f))
+            {
+                Debug.Log("Hit something! The" + hit.transform.gameObject.name + " didn't like that.");
+                if (Input.GetMouseButtonDown(0)) {
+
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+            else { Input.GetMouseButtonDown(1); Input.GetMouseButtonDown(2); }
+        }
+    }
+}
