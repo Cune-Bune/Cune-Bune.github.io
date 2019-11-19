318978ca8b8e88ebac2ac4f065d9f9f03c71dada
rosvany02 <rosvany.rodri@gmail.com>
2019-11-19 11:04:04 -0500

    Made Hook Script... Again.

--- Temporary Game 10.15/Assets/Scripts/Laser.cs
+++ Temporary Game 10.15/Assets/Scripts/Laser.cs
@@ -8,6 +8,7 @@ public class Laser : MonoBehaviour
     public Transform whatDoYouWantFromMe;
     public float multiplicationFactor = 5;
 	public float boomAmt = 50f;
+    public Transform player;
     public Transform target;
     // Start is called before the first frame update
     void Start()
@@ -17,19 +18,26 @@ public class Laser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-  if (Input.GetMouseButton(0))
-{
-RaycastHit hit = new RaycastHit();
-Ray laser = Camera.main.ScreenPointToRay(Input.mousePosition);
-if (Physics.Raycast(laser, out hit, 10000f))
-{     
-Debug.Log("Hit something! The " + hit.transform.gameObject.name + " didn't like that.");
-    //Destroy(hit.transform.gameObject); // destroy the object hit
-if (hit.rigidbody) {
-    rigidbody.AddForce((target.position - transform.position) * multiplicationFactor);
-    }
- }
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit = new RaycastHit();
+            Ray laser = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            if (Physics.Raycast(laser, out hit, 10000f))
+            {     
+                Debug.Log("Hit something! The " + hit.transform.gameObject.name + " didn't like that.");
+                //Destroy(hit.transform.gameObject); // destroy the object hit
+                
+                if (Vector3.Distance(target.position, player.position) < 10 && hit.rigidbody)
+                {
+                    target = hit.transform;
+                    player.GetComponent<Rigidbody>().AddForce((target.position - player.position) * multiplicationFactor);
+                }
+            }
         }
-            else { Input.GetMouseButton(1); Input.GetMouseButton(2);}     
+        else 
+        {
+            Input.GetMouseButton(1); Input.GetMouseButton(2);
+        }     
     }
 }
