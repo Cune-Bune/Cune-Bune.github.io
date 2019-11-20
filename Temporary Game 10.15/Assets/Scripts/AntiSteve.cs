fb872cfc0b23a66fd04b17f5bc124762e53fb2ec
Cune-Bune <Cune-Bune@users.noreply.github.com>
2019-11-20 16:18:04 -0500

    Whole bunch of stuff
    - Added AI
    - Added a new sort of obstacle course
    - Particles look nice
    - Clickable movement

---
+++ Temporary Game 10.15/Assets/Scripts/AntiSteve.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AntiSteve : MonoBehaviour
+{
+    NavMeshAgent joe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joe = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider triggerReport) // this will store information on collider trigger
+    {
+        Debug.Log("TRIGGER ENTERED");
+        if (triggerReport.name == "Steve") { //this checks the name of whatever thing entered the trigger zone. in this case steve.
+
+            Debug.Log("ENEMY SPOTTED");
+            joe.destination = triggerReport.transform.position;
+        } 
+    }
+}
---
+++ Temporary Game 10.15/Assets/Scripts/MyAgent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MyAgent : MonoBehaviour
+{
+    NavMeshAgent steve;
+    public Vector3 dest;//destination where steve goes
+    // Start is called before the first frame update
+    void Start()
+    {
+        steve = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        steve.destination = dest; //steve's destination is now dest
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit = new RaycastHit();
+            Ray laser = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(laser, out hit, 10000f))
+            {
+                Debug.Log("Hit something! The " + hit.transform.gameObject.name + " didn't like that.");
+                dest = hit.point;
+            }
+        }
+    }
+    /*else
+    {
+      Input.GetMouseButton(1); Input.GetMouseButton(2);
+    }*/
+//}
+}
