5a4b0190659a1d70d11515fd687daf92e058eab2
Cune-Bune <Cune-Bune@users.noreply.github.com>
2019-11-27 16:38:54 -0500

    FIXED ANIMATION FOR MOVEMENT IT ISNT TRASH ANYMORE I DID SOMETHING OOOOO

---
+++ Temporary Game 10.15/Assets/Scripts/FemAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FemAnim : MonoBehaviour
+{
+
+    Animator myAnim;
+
+    // Use this for initialization
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAnim.SetInteger("State", 0);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myAnim.SetInteger("State", 1);
+        }
+    }
+}
